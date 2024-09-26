0a2f2b7c7de8e87c91753b1d2600a1789a423f06
tamas.oarga <tamas.oarga@istructure.com>
2024-09-26 12:19:56 +0200

    Database setup with Postgres and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(databaseConfig())
   ],
   controllers: [AppController],
   providers: [AppService],
