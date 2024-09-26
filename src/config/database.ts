4f1543098d4a3185e8fbf41c306e3f53b53e6c1b
tamas.oarga <tamas.oarga@istructure.com>
2024-09-26 20:00:40 +0200

    MimeTypes enum reduced to fit into Postgres Enum. EntityScan also added to TypeORM config.

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,6 +11,11 @@ export default registerAs(
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     autoLoadEntities: true,
-    synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    //synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    synchronize: true,
+    migrationsRun: true,
+    entities: [
+      __dirname + '/../**/*.entity{.ts,.js}',
+    ],
   }),
 );
